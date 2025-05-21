d82e687017240df65bedff595dd802ebc79c6f9a
Shreyaupadhyay12 <150888428+Shreyaupadhyay12@users.noreply.github.com>
2025-05-21 10:34:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Placeholder JS – can be expanded to dynamically update data
+console.log("Weather App Loaded");
+
+// In future, you can fetch weather data from APIs like OpenWeatherMap or WeatherAPI
